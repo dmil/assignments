b013a253f3bdb2509acdd1a35ff92917b9e69d09
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2019-01-08 17:29:22 -0500

    add some comments explaing what sayhello does

--- sayhello.py
+++ sayhello.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-# This script reads from STDIN and greets the person whose name it gets passed in.
+# This script reads from STDIN
+#  and greets the person whose
+#  name it gets passed in.
 import sys
 name = sys.stdin.read()
 print("Hello " + name + "!")
