fb49c07e9d5cd5a3f8ac512dd4e105daa034f41c
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2019-01-16 13:13:07 -0500

    add exercise to read and write text file

--- test.py
+++ test.py
@@ -1,9 +1,39 @@
+import json
+# read superheroes.json
+with open('superheroes.json', 'r') as f:
+	superheroes = json.load(f)
+
+# write a csv with rows per member
 import csv
 
-with open('testwrite.csv', 'r') as f:
-    reader = csv.DictReader(f)
-    rows = list(reader)
+with open('superheroes.csv', 'w') as f:
+	writer = csv.writer(f)
 
-for row in rows:
-    item = dict(row) # Convert Ordered Dict to regular dict (python 3.6 or higher)
-    print(item)
+	# Write header
+	headers = ['name', 'age', 'secretIdentity',
+	'powers', 'squadName', 'homeTown', 'formed',
+	'secretBase', 'active','firstpower']
+	writer.writerow(headers)
+
+	# Loop over the members
+	members = superheroes['members']
+	for member in members:
+		# define variables
+		name = member['name']
+		age = member['age']
+		secretIdentity = member['secretIdentity']
+		powers = member['powers']
+
+		squadName = superheroes['squadName']
+		homeTown = superheroes['homeTown']
+		formed = superheroes['formed']
+		secretBase = superheroes['secretBase']
+		active = superheroes['active']
+
+		firstpower = member['powers'][0]
+
+		# write data to csv file
+		row = [
+			name, age, secretIdentity, powers, squadName,homeTown,formed,secretBase,active,firstpower
+		]
+		writer.writerow(row)
